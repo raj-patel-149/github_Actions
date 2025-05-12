2bd1cf4c4084ca7a895d60c2119dcf612fda896a
raj-patel-149 <rdpatel11124@gmail.com>
2025-05-12 12:06:57 +0530

    Modify hello.js

--- force-app/main/default/lwc/hello/hello.js
+++ force-app/main/default/lwc/hello/hello.js
@@ -1,6 +1,5 @@
 import { LightningElement } from "lwc";
 
 export default class Hello extends LightningElement {
-  name =
-    "Hello World i am raj and this is my first LWC component dev2 1234556655 dev1234dfdf";
+  name = "Hello World i am raj and this is dev branch from main";
 }
